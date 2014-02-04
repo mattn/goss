6ad318dbf1f851c8f4026532ecc4f3bf60775ff4
mattn <mattn.jp@gmail.com>
2014-02-04 11:00:32 +0900

    Fix usage

--- goss.go
+++ goss.go
@@ -14,7 +14,7 @@ import (
 
 func usage() {
 	fmt.Printf(`Usage:
-    goss ls sakura://<bucket>/path/to/file.txt
+    goss ls sakura://<bucket>/path/to/
     goss put sakura://<bucket>/path/to/ file.txt
     goss get sakura://<bucket>/path/to/file.txt
     goss cat sakura://<bucket>/path/to/file.txt
